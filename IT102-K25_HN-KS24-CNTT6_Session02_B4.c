49e1a47908e4ba7becabe13a63de648af27d5e0e
baogia2810 <bankaijun2880@gmail.com>
2025-07-29 07:38:05 +0700

    upload bài 4

---
+++ IT102-K25_HN-KS24-CNTT6_Session02_B4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int chieuDai = 5;
+    int chieuRong = 3;
+    int chuVi = 2 * (chieuDai + chieuRong);
+    int dienTich = chieuDai * chieuRong;
+
+    printf("Chu vi = %d\n", chuVi);
+    printf("Dien tich = %d\n", dienTich);
+
+    return 0;
+}
